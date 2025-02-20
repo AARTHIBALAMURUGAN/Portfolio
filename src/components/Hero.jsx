b226baadb74bc472d5e5f7865c3adb26017de5a8
AARTHI B <aarthib875@gmail.com>
2025-02-20 15:52:37 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,19 +29,26 @@ const Hero = () => {
                         A skilled Web developer with a strong portfolio, passionate about staying updated on frontend tech.
                         Collaborative and committed to quality, you thrive in dynamic teams. Eager to contribute creativity and bring innovative ideas to life.
                     </p>
-                    <button
-                        className="relative my-4 mx-auto px-5 py-2 text-xl w-fit text-white lg:mx-0
-             bg-gradient-to-r from-[#EC37D0] via-[#D92B2B] to-[#FFAA0D] 
-             bg-[length:400%] rounded-full transition-all duration-500 
-             hover:animate-gradientMove 
-             before:content-[''] before:absolute before:inset-0 
-             before:rounded-full before:bg-gradient-to-r before:opacity-0 
-             before:transition-opacity before:duration-500 hover:before:opacity-80 
-             hover:before:blur-[25px]"
-                        onClick={() => window.open('/HireMe.pdf', '_blank')}
-                    >
-                        Check Resume
-                    </button>
+                             <button
+    className="relative my-4 mx-auto px-5 py-2 text-xl w-fit text-white lg:mx-0
+        bg-gradient-to-r from-[#EC37D0] via-[#D92B2B] to-[#FFAA0D] 
+        bg-[length:400%] rounded-full transition-all duration-500 
+        hover:animate-gradientMove 
+        before:content-[''] before:absolute before:inset-0 
+        before:rounded-full before:bg-gradient-to-r before:opacity-0 
+        before:transition-opacity before:duration-500 hover:before:opacity-80 
+        hover:before:blur-[25px]"
+    onClick={() => {
+        const link = document.createElement("a");
+        link.href = "/HireMe.pdf"; // Ensure this file is in your public folder
+        link.download = "Aarthi_B_Resume.pdf"; // The name it will be saved as
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }}
+>
+    Check Resume
+</button>
                 </div>
             </div>
             <div className='w-full lg:w-[50%] flex justify-center items-center'>
